aa2d8076bed1a42bf484b1efdb38603fd8b24e62
Andres Pulido <andrespulido@ufl.edu>
2022-07-07 14:26:50 -0500

    improve update and resample step in PF

--- src/guidance.py
+++ src/guidance.py
@@ -8,11 +8,13 @@ from nav_msgs.msg import Odometry
 
 class guidance():
     def __init__(self):
+        ## Initialization of variables
         self.turtle_position = np.array([0, 0])
         self.turtle_pose = np.array([0, 0, 0])
         self.linear_velocity = np.array([0, 0])
         self.angular_velocity = np.array([0])
 
+        ## PARTICLE FILTER  ##
         # boundary of the lab [[x_min, y_min], [x_max, y_,max]] 
         AVL_dims = np.array([[-2, -2], [2, 2]])  
         # number of particles
@@ -27,7 +29,7 @@ class guidance():
         #self.particles_cumsum = np.cumsum(self.particles, axis=0)
         #self.particles_cumsum /= self.particles_cumsum[-1, :]
 
-        # ROS stuff
+        ## ROS stuff
         rospy.loginfo("Initializing markov_goal_pose node") 
         self.pose_pub = rospy.Publisher('desired_state', DesiredState, queue_size=1)
         self.turtle_odom_sub = rospy.Subscriber(
@@ -36,10 +38,67 @@ class guidance():
             '/multirotor/truth/NWU', Odometry, self.quad_odom_cb, queue_size=3)
         self.ds = DesiredState()
 
+    def particle_filter(self):
+        self.predict()
+        self.update()
+        self.resample()
+        self.pub_desired_state()
+        self.pose_pub.publish(self.ds)
 
     def predict(self):
+        """The predict step in the Bayes algorithm uses the process model to update the belief in the system state.
+        In our case, the process model is the motion of the turtlebot in 2D. In MML the predict step is a forward pass
+        on the NN.
+        Input: State of the particles
+        Output: Predicted state of the particles
+        """
         self.particles_pred_position = self.particles[:,:2] + self.linear_velocity*0.1
         self.particles_pred_angle = self.particles[:,2] + self.angular_velocity*0.1
+        # TODO: add noise to the predicted state
+        self.pred_pose = np.array([self.particles_pred_position[:,0], self.particles_pred_position[:,1], self.particles_pred_angle])
+
+    def update(self):
+        """The update step in the Bayes algorithm uses the measurement model to update the belief in the system state.
+        In our case, the measurement model is the position of the turtlebot in 2D. In MML the update step is the camera model.
+        Input: Likelihood of the particles from measurement model and prior belief of the particles
+        Output: Updated state of the particles
+        """
+        self.weights = self.get_weight(self.particles, self.turtle_pose)
+        self.weights_sum = np.sum(self.weights)
+        self.weights_cumsum = np.cumsum(self.weights)
+        self.weights_cumsum /= self.weights_sum
+        # TODO: finish update step
+    
+    def get_weight(self, particles, pose):
+        """Particles that closely match the measurements are weighted higher than
+        particles which don't match the measurements very well.
+        """
+        pos_weight = np.linalg.norm((particles[:,:2] - pose[:,:2]), axis=0)
+        angle_weight = np.abs(particles[:,2] - pose[:,2])
+        pos_weight = self.normalize(pos_weight)
+        angle_weight = self.normalize(angle_weight)
+        weight = pos_weight + angle_weight
+        # TODO: finish get_weight
+        return weight
+
+    def normalize(x):
+        """Normalize a vector to have L2 norm equal to 1"""
+        return (x - np.min(x)) / (np.max(x) - np.min(x))
+    
+    def resample(self):
+        """The resample step in the Bayes algorithm uses the resampling algorithm to update the belief in the system state.
+        In our case, the resampling algorithm is the systematic resampling.
+        Inputs: Updated state of the particles
+        Outputs: Updated state of the particles
+        """
+        self.particles = self.particles_cumsum[np.random.choice(self.N, self.N, p=self.weights_cumsum)]
+        self.weights_sum = np.sum(self.weights)
+        self.weights_cumsum = np.cumsum(self.weights)
+        self.weights_cumsum[-1] = 1.0  # avoid round-off errors
+        indexes = np.searchsorted(self.weights_cumsum, np.random.rand(self.N))
+        #self.weights_cumsum /= self.weights_sum
+        self.particles = self.particles[indexes]
+        self.weights = self.weights[indexes]
 
     def turtle_odom_cb(self, msg):
         self.turtle_position = np.array([msg.pose.pose.position.x, 
@@ -56,7 +115,7 @@ class guidance():
         _, _, theta_z = self.euler_from_quaternion(turtle_orientation)
         self.turtle_pose = np.array([self.turtle_position[0], self.turtle_position[1], theta_z]) 
 
-        self.predict()
+        self.particle_filter()
                                     
     @staticmethod
     def euler_from_quaternion(q):
@@ -82,8 +141,6 @@ class guidance():
     def quad_odom_cb(self, msg):
         self.quad_position = np.array([msg.pose.pose.position.x, 
                                          msg.pose.pose.position.y])
-        # Predict the next state
-        self.pub_desired_state()
         self.pose_pub.publish(self.ds)
     
     def pub_desired_state(self):
